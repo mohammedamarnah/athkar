5d83aef30b39108c87e59cd5b967d86a6a48c268
Mohammed Amarnah <m.amarnah@gmail.com>
2022-03-14 11:18:33 -0500

    replace google analytics with plausible

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-      caches.match(event.request).then(function(response) {
+      caches.match(event.request).then(response => {
           return response || fetch(event.request);
       })
   );
